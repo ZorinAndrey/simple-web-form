79d12ff030512ad9ba24a1f724b0626552d91109
AndreyZorin <azor@mail.ru>
2022-08-23 11:52:24 +0300

    Initial commit

---
+++ src/main/java/ru/azor/simple/web/form/SimpleWebFormApplication.java
@@ -0,0 +1,13 @@
+package ru.azor.simple.web.form;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/azor/simple/web/form/SimpleWebFormApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.azor.simple.web.form;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
